ef2d13ff19982db4f908c9753b890f193dce0c27
snaproom1992 <tsujiogawa1992@gmail.com>
2025-04-29 13:37:21 +0900

    Initial Commit

---
+++ KANJY/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KANJY
+//
+//  Created by 辻雄大 on 2025/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KANJY/KANJYApp.swift
@@ -0,0 +1,17 @@
+//
+//  KANJYApp.swift
+//  KANJY
+//
+//  Created by 辻雄大 on 2025/04/29.
+//
+
+import SwiftUI
+
+@main
+struct KANJYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
